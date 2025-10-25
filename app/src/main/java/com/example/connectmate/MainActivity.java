26bad8fefcc0633d0ff027d76d43d7fc1a7ba86a
seogjihyo <tapy1204@kookmin.ac.kr>
2025-10-25 18:15:17 +0900

    make activity_signup.xml make SignUpActivity.java make settings_activity.xml make bottom_navibar.xml modify MainActivity add bottomnavibar to activity_main.xml

--- app/src/main/java/com/example/connectmate/MainActivity.java
+++ app/src/main/java/com/example/connectmate/MainActivity.java
@@ -56,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
 
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.activity_main.xml), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
