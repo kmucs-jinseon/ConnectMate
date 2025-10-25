91d59ef2715adb12d26d09c64cb102efe680c8f2
Jenna <jenna@jenna-studio.dev>
2025-10-25 17:53:56 +0900

    Merge remote-tracking branch 'origin/master'

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,10 +41,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
-    implementation("androidx.core:core-splashscreen:1.0.1")
-    implementation("androidx.activity:activity:1.9.2")
-    implementation("androidx.lifecycle:lifecycle-runtime:2.8.4")
+
 }
+
