16407c4d2560f9ef3b5ff4c51be94c1a0a63cb55
seogjihyo <tapy1204@kookmin.ac.kr>
2025-10-25 17:54:28 +0900

    make activity_signup.xml make SignUpActivity.java

---
+++ app/src/main/java/com/example/connectmate/ChatListFragment.java
@@ -0,0 +1,6 @@
+package com.example.connectmate;
+
+import androidx.fragment.app.Fragment;
+public class ChatListFragment extends Fragment {
+    public ChatListFragment() { super(R.layout.fragment_chat); }
+}
